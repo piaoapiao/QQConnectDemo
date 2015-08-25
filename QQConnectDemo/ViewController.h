cbdbfc542e1f206fff772a7f8c8ccc2fee7dc0c3
piaoapiao <guodong@guodongdeMacBook-Pro.local>
2015-08-25 16:48:55 +0800

    Initial Commit

---
+++ QQConnectDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQConnectDemo
+//
+//  Created by guodong on 15/8/25.
+//  Copyright (c) 2015年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
