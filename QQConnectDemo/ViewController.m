2287df53391692c2595110fcd020bc7c6162c673
piaoapiao <guodong@guodongdeMacBook-Pro.local>
2015-08-25 16:58:03 +0800

    share qq

--- QQConnectDemo/ViewController.m
+++ QQConnectDemo/ViewController.m
@@ -8,6 +8,10 @@
 
 #import "ViewController.h"
 
+
+#import <TencentOpenAPI/TencentOAuth.h>
+#import "TencentOpenAPI/QQApiInterface.h"
+
 @interface ViewController ()
 
 @end
@@ -22,6 +26,75 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)click:(id)sender {
+
+        NSString *appid = @"1104255576";
+        [[TencentOAuth alloc] initWithAppId:appid andDelegate:nil];
+
+
+            QQApiTextObject* txtObj = [QQApiTextObject objectWithText:@"123"];
+            SendMessageToQQReq* req = [SendMessageToQQReq reqWithContent:txtObj];
+    
+    
+    
+            QQApiSendResultCode sent = [QQApiInterface sendReq:req];
+    
+            [self handleSendResult:sent];
+}
+
+- (void)handleSendResult:(QQApiSendResultCode)sendResult
+{
+    switch (sendResult)
+    {
+        case EQQAPIAPPNOTREGISTED:
+        {
+            UIAlertView *msgbox = [[UIAlertView alloc] initWithTitle:@"Error" message:@"App未注册" delegate:nil cancelButtonTitle:@"取消" otherButtonTitles:nil];
+            [msgbox show];
+            //[msgbox release];
+            
+            break;
+        }
+        case EQQAPIMESSAGECONTENTINVALID:
+        case EQQAPIMESSAGECONTENTNULL:
+        case EQQAPIMESSAGETYPEINVALID:
+        {
+            UIAlertView *msgbox = [[UIAlertView alloc] initWithTitle:@"Error" message:@"发送参数错误" delegate:nil cancelButtonTitle:@"取消" otherButtonTitles:nil];
+            [msgbox show];
+            //[msgbox release];
+            
+            break;
+        }
+        case EQQAPIQQNOTINSTALLED:
+        {
+            UIAlertView *msgbox = [[UIAlertView alloc] initWithTitle:@"Error" message:@"未安装手Q" delegate:nil cancelButtonTitle:@"取消" otherButtonTitles:nil];
+            [msgbox show];
+           // [msgbox release];
+            
+            break;
+        }
+        case EQQAPIQQNOTSUPPORTAPI:
+        {
+            UIAlertView *msgbox = [[UIAlertView alloc] initWithTitle:@"Error" message:@"API接口不支持" delegate:nil cancelButtonTitle:@"取消" otherButtonTitles:nil];
+            [msgbox show];
+          //  [msgbox release];
+            
+            break;
+        }
+        case EQQAPISENDFAILD:
+        {
+            UIAlertView *msgbox = [[UIAlertView alloc] initWithTitle:@"Error" message:@"发送失败" delegate:nil cancelButtonTitle:@"取消" otherButtonTitles:nil];
+            [msgbox show];
+           // [msgbox release];
+            
+            break;
+        }
+        default:
+        {
+            break;
+        }
+    }
 }
 
 @end
